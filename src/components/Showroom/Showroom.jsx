d8eeace1b4739ac7f569dfa95d1a769182f39ed4
omertaskn452 <omer_taskn@outlook.com>
2024-03-25 21:31:45 +0300

    Carousel updated

--- src/components/Showroom/Showroom.jsx
+++ src/components/Showroom/Showroom.jsx
@@ -2,21 +2,21 @@ import React, { useState } from "react";
 import style from "./showroom.css"
 import {CircleArrowLeft, CircleArrowRight} from "lucide-react";
 
-export default function Showroom({selectedSlide, index, tours, previousSlide, nextSlide}) {
+export default function Showroom({moveSlider, index, tours}) {
 
 
   return(
-    <div className="showroom" style={{translate: `${-100 * selectedSlide}%`}}>
+    <div className="showroom">
       <div className="showroom-img-wrapper">
         <img className="showroom-img" src={tours[index].img} alt="" />
       </div>
       <div className="showroom-info">
         <div className="showroom-info-transition">
-          <div onClick={() => previousSlide()} className="showroom-info-transition-btn">
+          <div onClick={() => moveSlider(-1)} className="showroom-info-transition-btn">
             <CircleArrowLeft className="transition-btn" size={32}/>
             <span className="showroom-trns-btn-text hideOnMobile">Previous Deals</span>
           </div>
-          <div onClick={() => nextSlide()} className="showroom-info-transition-btn">
+          <div onClick={() => moveSlider(1)} className="showroom-info-transition-btn">
             <span className="showroom-trns-btn-text align-left hideOnMobile">Next Deals</span>
             <CircleArrowRight  className="transition-btn" size={32}/>
           </div>
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,19 +1,54 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import style from "./slider.css"
 import Showroom from "../Showroom/Showroom";
 import toursData from "../../tourData"
 
 export default function Slider() {
 
-  const [selectedSlide, setSelectedSlide] = useState(0)
+  const carouselRef = useRef()
 
-  const nextSlide = () => {
-    selectedSlide === toursData.categories.length - 1 ? setSelectedSlide(0) : setSelectedSlide(prevState => prevState + 1) 
+  const [count, setCount] = useState(0)
+  const [touchStartX, setTouchStartX] = useState(null)
+  const [touchEndX, setTouchEndX] = useState(null)
+
+  const handleTouchStart = (e) =>{
+    setTouchEndX(null)
+    setTouchStartX(e.targetTouches[0].clientX)
   }
 
-  const previousSlide = () => {
-    selectedSlide === 0 ? setSelectedSlide((toursData.categories.length - 1)) : setSelectedSlide(prevState => prevState - 1)
+  const handleTouchMove = (e) => setTouchEndX(e.targetTouches[0].clientX)
+
+  const handleTouchEnd = (e) => {
+    if(!touchEndX || !touchStartX) return;
+    const delta = touchStartX - touchEndX
+    if(delta > 50){
+      moveSlider(1)
+    }
+    else if(delta < -50){
+      moveSlider(-1)
+    }
   }
+
+  const moveSlider = (delta) => {
+    if(!carouselRef.current) return
+    
+    const width = carouselRef.current.offsetWidth;
+
+    if(count + delta > (toursData.categories.length - 1)){
+      setCount(0)
+      carouselRef.current.scrollTo(0 ,0)
+    }
+    else if(count + delta < 0){
+      setCount(toursData.categories.length - 1)
+      carouselRef.current.scrollTo(carouselRef.current.scrollLeft + width * (toursData.categories.length - 1), 0)
+    }
+    else{
+    setCount(prevState => prevState + delta)
+    carouselRef.current.scrollTo(carouselRef.current.scrollLeft + width * delta, 0)
+    }
+  }
+
+  console.log(count)
 
   let sliderItems = toursData.categories.map((item, index) => {
     return <Showroom
@@ -21,16 +56,20 @@ export default function Slider() {
       key={index}
       index={index}
       tours={toursData.categories}
-      selectedSlide={selectedSlide}
-      nextSlide={nextSlide}
-      previousSlide={previousSlide}
+      moveSlider={moveSlider}
     />
   })
 
   return(
       <div className="container">
-        <div className="slider">
-          {sliderItems}
+        <div 
+          className="slider" 
+          ref={carouselRef}
+          onTouchStart={handleTouchStart}
+          onTouchMove={handleTouchMove}
+          onTouchEnd={handleTouchEnd}
+        >
+            {sliderItems}
         </div>
       </div>
   )
